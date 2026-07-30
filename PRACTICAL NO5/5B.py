88c676377b2a778e552cac14b38d669750b93781
F103 PREETHI NARESH <nareshkumarpreethics252623@gmail.com>
2026-07-30 21:49:52 +0530

    Update 5B.py

--- PRACTICAL NO5/5B.py
+++ PRACTICAL NO5/5B.py
@@ -1,30 +1,20 @@
-# Practical No. 5 (5d)
+# Practical No. 5 (5b)
 # Name : Preethi Naresh
 # Roll No : S103
 
 import pandas as pd
 
 print("=" * 40)
-print("PRACTICAL NO. 5 (5d)")
+print("PRACTICAL NO. 5 (5b)")
 print("=" * 40)
 print("Name     : Preethi Naresh")
-print("Roll No. : S103 ")
+print("Roll No. : S103")
 print()
 
-# Create a Pandas Series
-marks = pd.Series([45, 68, 72, 55, 89, 91, 60])
+# Read Excel file
+df = pd.read_excel("StressLevelDataset.xlsx")
 
-print("Original Series:")
-print(marks)
+print("Statistical Information of Dataset")
+print()
 
-# Create a Boolean array
-boolean_array = marks >= 70
-
-print("\nBoolean Array:")
-print(boolean_array)
-
-# Filter the Series using the Boolean array
-filtered_marks = marks[boolean_array]
-
-print("\nFiltered Series (Marks >= 70):")
-print(filtered_marks)
+print(df.describe())
