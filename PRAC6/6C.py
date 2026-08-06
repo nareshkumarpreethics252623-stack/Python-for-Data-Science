762a42e941224543ec21b6a53bcbf320727411d4
F103 PREETHI NARESH <nareshkumarpreethics252623@gmail.com>
2026-08-06 20:03:20 +0530

    Create 6C.py

---
+++ PRAC6/6C.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+# Data
+categories = ["Data Structures", "Scala for DS", "Operating System", "Python for DS"]
+scores = [65, 70, 74, 60]
+
+plt.bar(categories, scores)
+
+plt.title("Student Scores")
+plt.xlabel("Subjects")
+plt.ylabel("Scores")
+
+# Display the chart
+plt.show()
